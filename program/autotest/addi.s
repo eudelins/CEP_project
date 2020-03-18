545c3f57723cd768c2fad92dd0f082c02b08045f
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-18 18:38:46 +0000

    addi

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -2,17 +2,17 @@
 	.text
 
   # FFFFF800
+  # 000003C1
   # le registre x0 contient toujours 0
   # syntaxe: addi rd, rs1, imm
   # imm est codé sur 11 bits et est donc ompris entre -2048 et 2047
 
   addi x31, x0, 0    # test: stock 0 dans le registre 31
-  addi x31, x0, 961  # test: son stock un nb quelconque dans x31
+  # addi x31, x0, 961  # test: son stock un nb quelconque dans x31
   # addi x31, x0, -2048   # test: valeur extreme de imm
 
 
 	# max_cycle 50
 	# pout_start
 	# 00000000
-	# 000003C1
 	# pout_end
