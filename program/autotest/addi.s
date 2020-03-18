137cf56e2b1116ba4d84cc6757fa91544d91004c
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-18 18:45:56 +0000

    addi

--- program/autotest/addi.s
+++ program/autotest/addi.s
@@ -1,7 +1,6 @@
 # TAG = addi
 	.text
 
-  # FFFFF800
   # 000003C1
   # le registre x0 contient toujours 0
   # syntaxe: addi rd, rs1, imm
@@ -9,10 +8,11 @@
 
   addi x31, x0, 0    # test: stock 0 dans le registre 31
   # addi x31, x0, 961  # test: son stock un nb quelconque dans x31
-  # addi x31, x0, -2048   # test: valeur extreme de imm
+  addi x31, x0, -2048   # test: valeur extreme de imm
 
 
 	# max_cycle 50
 	# pout_start
 	# 00000000
+  # F8000000
 	# pout_end
