a2eafe18bc9728b02eae0909ad1eb07885409b0d
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-23 13:22:22 +0000

    bug

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -3,17 +3,17 @@
 
   # le registre x0 contient toujours 0
   # syntaxe: auipc rd, imm
-  # 00002010
 
   auipc x31, 0  # test: chargement de la valeur de PC
   auipc x31, 0  # test: chargement de la valeur de PC
   addi x0, x0, 0  # On effectue une instruction quelconque pour augmenter PC de 4
-  auipc x31, 0  # test: chargement de la valeur de PC, on vérifie l'augmentation de 8
-  # auipc x31, 0x00001  # test: addition avec un immédiat quelconque
+  auipc x31, 0  # test: chargement de la valeur de PC, on vérifie l'augmentation de 4
+  auipc x31, 0x00001  # test: addition avec un immédiat quelconque
 
 	# max_cycle 50
 	# pout_start
 	# 00001000
   # 00001004
-  # 0000100C
+  # 00001008
+  # 00002010
 	# pout_end
