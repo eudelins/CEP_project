467222bbc38e4436ad7ddb70699dba7482ab42eb
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-23 11:07:22 +0000

    bug

--- program/autotest/add.s
+++ program/autotest/add.s
@@ -2,7 +2,7 @@
 	.text
 
   # le registre x0 contient toujours 0
-  # syntaxe: addi rd, rs1, imm
+  # syntaxe: add rd, rs1, rs2
   # imm est codé sur 11 bits et est donc ompris entre -2048 et 2047
 
   add x31, x0, x0    # test: stock 0 dans le registre 31
@@ -13,6 +13,5 @@
 	# max_cycle 50
 	# pout_start
 	# 00000000
-  # 00000000
   # FFFFF000
 	# pout_end
