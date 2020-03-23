d919a3951027f9f9859d3291d4922b970a97acca
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-23 11:41:03 +0000

    ajout de auipc.s

---
+++ program/autotest/auipc.s
@@ -0,0 +1,12 @@
+# TAG = auipc
+	.text
+
+  # le registre x0 contient toujours 0
+  # syntaxe: auipc rd, imm
+
+	auipc x31, 0  # test: chargement de la valeur de PC
+
+	# max_cycle 50
+	# pout_start
+	# 00000FA0
+	# pout_end
