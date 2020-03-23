e32e875c207d7ff1e9a8b69278a35565a38c20db
Elodie Boulhol <elodie.boulhol@grenoble-inp.org>
2020-03-23 17:21:20 +0100

    Replace sll.s

--- program/autotest/sll.s
+++ program/autotest/sll.s
@@ -6,20 +6,36 @@
   # les 5 dernier bits de poids faible de rs2 sont compris entre 0 et 31
 	# rs1 est compris entre -2147483648 et 2147483647
 
+	sll x31, x0, x0								# test: valeur extrême inf de x2 stockée dans le registre 31
 
-	sll x31, x0, 0								# test: valeur extrême inf de x1 stockée dans le registre 31
-	sll x31, x0, 31								# test: valeur extrême sup de x1
-	sll x31, -2147483648, 31			# test: valeur extrême inf de x0
-	sll x31, 2147483647, 0				# test: valeur extrême sup de x0
-	sll x31, 2945, 8							# test: stock x0 décalé de 8 bits vers la gauche dans le registre 31
+	addi x2, x0, 31								# chargement constante
+	sll x31, x0, x2								# test: valeur extrême sup de rs2
+
+	addi x1, x0, -2147483648			# chargement constante
+	addi x2, x0, 31								# chargement constante
+	sll x31, x1, x2								# test: valeur extrême inf de rs1
+
+	addi x1, x0, 2147483647				# chargement constante
+	addi x2, x0, 31								# chargement constante
+	sll x31, x1, x2								# test: valeur extrême sup de rs2
+
+	sll x31, x1, x0								# test: valeur extrême inf de rs2
+
+	addi x1, x0, 2945							# chargement constante
+	addi x2, x0, 8								# chargement constante
+	sll x31, x1, x2								# test: stock x0 décalé de 8 bits vers la gauche dans le registre 31
 																		# les 8 bits de poids faibles étants remplacés par des 0
-	sll x31, -393956, 22								#test: idem pour un décalage de 22 bits
+
+	addi x1, x0, -393956					# chargement constante
+	addi x2, x0, 22								# chargement constante
+	sll x31, x1, x2								# test: idem pour un décalage de 22 bits
 
 
 	# max_cycle 50
 	# pout_start
 	# 00000000
   # 00000000
+  #	00000000
   #	00000000
   #	7FFFFFFF
 	# 000B8100
