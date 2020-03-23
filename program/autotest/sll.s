5f12407e553196816f9ef83f43cc59ddcb3eb855
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-23 20:14:15 +0000

    bug

--- program/autotest/sll.s
+++ program/autotest/sll.s
@@ -17,14 +17,16 @@
 
 	sll x31, x1, x0								# test: valeur extrême inf de rs2
 
-	addi x1, x0, 2045							# chargement constante aléatoire postive
-	addi x2, x0, 8								# chargement constante
-	sll x31, x1, x2								# test: stock x0 décalé de 8 bits vers la gauche dans le registre 31
+	# addi x1, x0, 2045							# chargement constante aléatoire postive
+	# addi x2, x0, 8								# chargement constante
+	# sll x31, x1, x2								# test: stock x0 décalé de 8 bits vers la gauche dans le registre 31
+	# FFFFFE77
 																		# les 8 bits de poids faibles étants remplacés par des 0
 
-	addi x1, x0, -393 						# chargement constante aléatoire négative
-	addi x2, x0, 22								# chargement constante
-	sll x31, x1, x2								# test: idem pour un décalage de 22 bits
+	# addi x1, x0, -393 						# chargement constante aléatoire négative
+	# addi x2, x0, 22								# chargement constante
+	# sll x31, x1, x2								# test: idem pour un décalage de 22 bits
+	# 000007FD
 
 
 	# max_cycle 50
@@ -33,6 +35,4 @@
   # 00000000
   #	00000000
 	# 00000001
-  # FFFFFE77
-	# 000007FD
 	# pout_end
