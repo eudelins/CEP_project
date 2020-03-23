7c434c4970ede39684ae0ff773b568f5425bf203
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-23 11:13:22 +0000

    test_add

--- program/autotest/add.s
+++ program/autotest/add.s
@@ -8,10 +8,17 @@
   add x31, x0, x0    # test: stock 0 dans le registre 31
   lui x5, 0xfffff
   add x31, x0, x5  # test: 0 + un nb quelconque
+  addi x5, x0, 961
+  addi x6, x0, 1
+  add x31, x5, x6  # test: incrémentation de x5 stocké dans x6
+  addi x6, x0, -256
+  add x31, x5, x6  # test: somme de 2 nb quelconque
 
 
 	# max_cycle 50
 	# pout_start
 	# 00000000
   # FFFFF000
+  # 000003C2
+  # 000002C1
 	# pout_end
