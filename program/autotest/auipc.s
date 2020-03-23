556d684349685505f9148d3a769c5475d33a5bc0
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-23 12:47:06 +0000

    test auipc

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -5,8 +5,15 @@
   # syntaxe: auipc rd, imm
 
 	auipc x31, 0  # test: chargement de la valeur de PC
+  auipc x31, 0  # test: chargement de la valeur de PC, on vérifie l'augmentation de 4
+  lui x0, x0, 0  # On effectue une instruction quelconque pour augmenter PC de 4
+  auipc x31, 0  # test: chargement de la valeur de PC, on vérifie l'augmentation de 8
+  auipc x31, -10  # test: addition avec un immédiat quelconque
 
 	# max_cycle 50
 	# pout_start
 	# 00001000
+  # 00001004
+  # 0000100C
+  # FFFF8000
 	# pout_end
