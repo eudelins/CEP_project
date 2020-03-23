ffc7a5b3424134f868081b0f5b39b33dbe29beea
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-03-23 13:03:11 +0000

    bug

--- program/autotest/auipc.s
+++ program/autotest/auipc.s
@@ -7,6 +7,7 @@
   # 00002010
 
   auipc x31, 0  # test: chargement de la valeur de PC
+  auipc x31, 0  # test: chargement de la valeur de PC
   auipc x31, 0  # test: chargement de la valeur de PC, on vérifie l'augmentation de 4
   # addi x0, x0, 0  # On effectue une instruction quelconque pour augmenter PC de 4
   # auipc x31, 0  # test: chargement de la valeur de PC, on vérifie l'augmentation de 8
@@ -15,6 +16,7 @@
 	# max_cycle 50
 	# pout_start
 	# 00001000
+  # 00001000
   # 00001004
 
 	# pout_end
