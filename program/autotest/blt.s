16e806c4fdefa26ec7a92d06896f01ec19b7c36b
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-04-05 21:20:44 +0100

    Test

---
+++ program/autotest/blt.s
@@ -0,0 +1,26 @@
+# TAG = blt
+	.text
+
+  # le registre x0 contient toujours 0
+  # syntaxe: blt rs1, rs2, label
+  # opération: rs1 < rs2 => pc <- pc + cst
+
+
+	addi x1, x0, 1
+  blt x1, x0, plus  # test si rs1 >= rs2
+  auipc x31, 0
+  blt x0, x1, plus  # test si rs1 < rs2
+  blt x0, x1, chargement
+
+plus:
+  add x31, x1, x0
+
+chargement:
+  lui x31, 0xfffff
+
+	# max_cycle 50
+	# pout_start
+	# 00001008
+  # 00000001
+	# FFFFF000
+	# pout_end
