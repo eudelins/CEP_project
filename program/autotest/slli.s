62c37824333298d1d55ef6a75ea836d463c4830c
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-04-05 18:33:40 +0100

    Test slli

---
+++ program/autotest/slli.s
@@ -0,0 +1,25 @@
+# TAG = slli
+	.text
+
+  # le registre x0 contient toujours 0
+  # syntaxe: sll rd, rs1, imm
+
+
+	slli x31, x0, 0
+
+  addi x1, x0, 10
+	slli x31, x1, 0
+
+	addi x1, x0, 2045
+	sll x31, x1, 8
+
+	addi x1, x0, -393
+	sll x31, x1, 22								
+
+	# max_cycle 50
+	# pout_start
+	# 00000000
+	# 0000000A
+	# 0007FD00
+	# 9DC00000
+	# pout_end
--- vhd/CPU_PC.vhd
+++ vhd/CPU_PC.vhd
@@ -45,7 +45,8 @@ architecture RTL of CPU_PC is
       S_SUB,
       S_SRL,
       S_SRA,
-      S_SRAI
+      S_SRAI,
+      S_SLLI
       );
 
     signal state_d, state_q : State_type;
@@ -222,6 +223,12 @@ begin
               cmd.PC_sel <= PC_from_pc;
               cmd.PC_we <= '1';
               state_d <= S_SRAI;
+            elsif status.IR(31 downto 25) = "0000000" and status.IR(14 downto 12) = "001" and status.IR(6 downto 0) = "0010011" then
+              -- PC <- PC + 4
+              cmd.TO_PC_Y_sel <= TO_PC_Y_cst_x04;
+              cmd.PC_sel <= PC_from_pc;
+              cmd.PC_we <= '1';
+              state_d <= S_SLLI;
             elsif status.IR(14 downto 12) = "000" and status.IR(6 downto 0) = "1100011" then
               state_d <= S_BEQ1;
             elsif status.IR(14 downto 12) = "010" and status.IR(6 downto 0) = "0110011" then
@@ -316,6 +323,21 @@ begin
           when S_SLL =>
               -- rd <- rs1 << rs2(0:4)
               cmd.SHIFTER_Y_sel <= SHIFTER_Y_rs2;
+              cmd.SHIFTER_op <= SHIFT_ll;
+              cmd.DATA_sel <= DATA_from_shifter;
+              cmd.RF_we <= '1';
+              -- lecture mem[PC]
+              cmd.ADDR_sel <= ADDR_from_pc;
+              cmd.mem_ce <= '1';
+              cmd.mem_we <= '0';
+              -- next state
+              state_d <= S_Fetch;
+
+
+
+          when S_SLLI =>
+            -- rd <- rs1 << rs2(0:4)
+              cmd.SHIFTER_Y_sel <= SHIFTER_Y_ir_sh;
               cmd.SHIFTER_op <= SHIFT_ll;
               cmd.DATA_sel <= DATA_from_shifter;
               cmd.RF_we <= '1';
