8f98c6b938bf5993e73691cc2a923910960c60bf
boulhole <elodie.boulhol@grenoble-inp.org>
2020-04-06 13:12:39 +0200

    ajout du test de jal

---
+++ program/autotest/jal.s
@@ -0,0 +1,28 @@
+# TAG = jal
+	.text
+
+  # le registre x0 contient toujours 0
+  # syntaxe: jal rd, label
+  # opérations: rd <- PC + 4, PC <- PC + 4
+  # PC vaut initialement 0x00001000
+
+label1:
+  lui x31, OxOO000
+
+label2;
+  lui x31, 0xfffff
+
+jal x31, label1   # x31 doit valoir PC + 4
+auipc x31, 0      # x31 doit valoir PC + label1 = PC
+
+jal x31, label2   # x31 doit valoir PC + 4
+auipc x31, 0      # x31 doit valoir PC + label2 = PC + Oxfffff
+
+
+	# max_cycle 50
+	# pout_start
+	# 00001004
+  # 00001004
+  # 0000100C
+  # OOOOOOOC
+	# pout_end
