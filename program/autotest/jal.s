11206d1a2a1b0b64c37bb84a64ff0ee6ca56599d
boulhole <elodie.boulhol@grenoble-inp.org>
2020-04-06 16:01:06 +0200

    rectification des bugs

--- program/autotest/jal.s
+++ program/autotest/jal.s
@@ -6,21 +6,15 @@
   # opérations: rd <- PC + 4, PC <- PC + 4
   # PC vaut initialement 0x00001000
 
-label1:
-  lui x31, 0x00000
 
-label2:
-  lui x31, 0xfffff
+label1:
+  lui x31, 0xff320
 
 jal x31, label1   # x31 doit valoir PC + 4
-
-jal x31, label2   # x31 doit valoir PC + 4
 
 
 	# max_cycle 50
 	# pout_start
 	# 00001004
-  # 00000000
-  # 0000100C
-  # FFFFF000
+  # FF320000
 	# pout_end
