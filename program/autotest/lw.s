eb9f80d15553e6a44abbd6df129f65c15a7c4b61
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-04-06 08:50:15 +0100

    Test

--- program/autotest/lw.s
+++ program/autotest/lw.s
@@ -8,10 +8,13 @@
   addi x1, x0, 50
   sw x0, 0(x1)
   lw x31, 0(x1)
-
+  lui x2, 0xABCDE
+  sw x2, 0(x1)
+  lw x31, 0(x1)
 
 
 	# max_cycle 50
 	# pout_start
 	# 00000000
+  # ABCDE000
 	# pout_end
