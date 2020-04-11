fda8b0150f9a8595f195082d2f9e7502cdbc8745
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-04-11 15:42:51 +0000

    Test

--- program/autotest/jalr.s
+++ program/autotest/jalr.s
@@ -7,12 +7,14 @@
   # PC vaut initialement 0x00001000
 
 
-jalr x31, 16(x0)   # x31 doit valoir PC + 4
+lui x1, 0x00002
+jalr x31, 8(x1)   # x31 doit valoir PC + 4
 auipc x31, 0
-
+addi x0, x0, 0
+addi x31, x0, 16
 
 	# max_cycle 50
 	# pout_start
-	# 00001004
+	# 00001008
     # 00000010
 	# pout_end
