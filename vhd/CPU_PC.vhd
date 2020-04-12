4d8a2771b8262b91c53f06a06e0ef78260ec16a4
boulhole <elodie.boulhol@grenoble-inp.org>
2020-04-12 20:44:24 +0200

    erreurs de syntaxe

--- vhd/CPU_PC.vhd
+++ vhd/CPU_PC.vhd
@@ -790,33 +790,33 @@ begin
             state_d <= S_Fetch;
 
 
-            when S_LHU1 =>
-                -- calcul de l'@ qui vaut immI + rs1
-                cmd.AD_Y_select <= AD_Y_immI;
-                cmd.AD_we <= '1';
-                -- next state
-                state_d <= S_LHU2;
+          when S_LHU1 =>
+            -- calcul de l'@ qui vaut immI + rs1
+            cmd.AD_Y_select <= AD_Y_immI;
+            cmd.AD_we <= '1';
+            -- next state
+            state_d <= S_LHU2;
 
-            when S_LHU2 =>
-                -- lecture de la mémoire à l'@
-                cmd.ADDR_sel <= ADDR_from_ad;
-                cmd.mem_ce <= '1';
-                cmd.mem_we <= '0';
-                -- next state
-                state_d <= S_LHU3;
+          when S_LHU2 =>
+            -- lecture de la mémoire à l'@
+            cmd.ADDR_sel <= ADDR_from_ad;
+            cmd.mem_ce <= '1';
+            cmd.mem_we <= '0';
+            -- next state
+            state_d <= S_LHU3;
 
-            when S_LHU3 =>
-                -- rd <- mem[@]
-                cmd.RF_SIZE_sel <= RF_SIZE_half;
-                cmd.RF_SIGN_enable <= '0';
-                cmd.DATA_sel <= DATA_from_mem,
-                cmd.RF_we <= '1';
-                -- lecture de la mémoire à PC
-                cmd.ADDR_sel <= ADDR_from_pc;
-                cmd.mem_ce <= '1';
-                cmd.mem_we <= '0';
-                -- next state
-                state_d <= S_Fetch;
+          when S_LHU3 =>
+            -- rd <- mem[@]
+            cmd.RF_SIZE_sel <= RF_SIZE_half;
+            cmd.RF_SIGN_enable <= '0';
+            cmd.DATA_sel <= DATA_from_mem;
+            cmd.RF_we <= '1';
+            -- lecture de la mémoire à PC
+            cmd.ADDR_sel <= ADDR_from_pc;
+            cmd.mem_ce <= '1';
+            cmd.mem_we <= '0';
+            -- next state
+            state_d <= S_Fetch;
 
 
 ---------- Instructions de sauvegarde en mémoire ----------
