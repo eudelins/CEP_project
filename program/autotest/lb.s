12ec6b56157bd71642c434b5a0de90471905ccf3
boulhole <elodie.boulhol@grenoble-inp.org>
2020-04-12 15:47:58 +0200

    mise en palce du test

--- program/autotest/lb.s
+++ program/autotest/lb.s
@@ -1,42 +1,21 @@
 # TAG = lb
-	.text
 
   # le registre x0 contient toujours 0
   # syntaxe: lb rd, imm(rs1)
   # opérations: rd <- mem[immI + rs1]7...0 + extension de signe sur les 24 bits de poids faible
-  
-
-lb x31, 
-
-	# max_cycle 50
-	# pout_start
-	# 
-    # 
-	# pout_end
-
 
 
   .data
 
-mot : .word 50
+mot : .word 34
 
-	.text
+  .text
 
-  # le registre x0 contient toujours 0
-  # syntaxe: lw rd, imm(rs1)
-  # opération: rd <- mem[rs1 + immI]
-
-  add x0, x0, x0
-  lui x1, 1
-  lw x31, 0(x1)
-
-  la x1, mot
-  lui x2, 0x0BCDE
-  sw x2, 0(x1)
-  lw x31, 0(x1)
+  la x1, mot   # dans x1 on met l'@ de mot
+  lb x31, 0(x1)
 
 	# max_cycle 50
 	# pout_start
-  # 00000033
-  # 0BCDE000
-  # pout_end
+    # 00000022
+	# pout_end
+
