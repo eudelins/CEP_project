38920759c213b60f3356b8f75a1f9d11f01c0c84
Simon Eudeline <simon.eudeline@grenoble-inp.org>
2020-04-27 15:00:07 +0200

    test

--- program/autotest/jalr.s
+++ program/autotest/jalr.s
@@ -12,15 +12,10 @@ mot : .word 50
   # PC vaut initialement 0x00001000
 
 la x3, mot
-addi x1, x0, 307  # stock l'instruction add x2, x0, x0, dans x1
+addi x1, x0, 4019  # stock l'instruction add x31, x0, x0, dans x1
 sw x1, 0(x3)  # stock l'instruction add x2, x0, x0, dans mot
 jalr x31, 0(x1)   # x31 doit valoir PC + 4
 
-# lui x3, 0x00003
-# sw x1, 0(x3),   # stock l'instruction add x2, x0, x0 à l'adresse 3000
-# auipc x31, 0
-# addi x0, x0, 0
-# addi x31, x0, 16
 
 	# max_cycle 50
 	# pout_start
